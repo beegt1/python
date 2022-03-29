1290fe7d0a9ab4fed6e84ff0cbc7d906eafbcf35
beegt1 <92780370+beegt1@users.noreply.github.com>
2022-03-29 14:49:23 +1100

    Create dns_enumeration.py

---
+++ dns_enumeration.py
@@ -0,0 +1,23 @@
+## To run, python3 ./dns_enumeration.py example.com 
+## To run and export to a .txt file, python3 ./dns_enumeration.py example.com > example.txt
+## if running the export way, be mindful it will not display results on the terminal - but in the file at the end.
+
+import dns.resolver 
+import sys
+
+
+domain = sys.argv[1]
+record_types = ['A', 'AAAA', 'NS', 'CNAME', 'MX', 'PTR', 'SOA', 'TXT']
+for records in record_types:
+    try:
+        answers = dns.resolver.resolve(domain, records)
+        print(f'\n{records} Records')
+        print('-'*30)
+
+        for server in answers:
+
+            print(server.to_text())
+    except dns.resolver.NoAnswer:
+        pass
+    except KeyboardInterrupt:
+        quit()
